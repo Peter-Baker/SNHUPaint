b4bc9c2990720386d732e20d9818fd0e2fd3b904
PeterBaker0213 <peter.baker@snhu.edu>
2020-02-15 12:53:26 -0500

    Able to Draw Now

--- GUI_Layout.py
+++ GUI_Layout.py
@@ -1,12 +1,11 @@
-import kivy
-
 from kivy.app import App
 
-from kivy.uix.image import Image
-from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.tabbedpanel import TabbedPanel
+
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse, Line
 
 Builder.load_string("""
 
@@ -173,16 +172,27 @@ Builder.load_string("""
             text: 'Edit tab content will be here'
 """)
 
+
 class Test(TabbedPanel):
-    pass
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(0, 0, 0)
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+
 
 class MyWidget(BoxLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+
 class CanvasApp(App):
+
     def build(self):
         return Test()
+
 
 CanvasApp().run()
