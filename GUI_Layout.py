d826c7604d3546f9df0f9b7fae287d07b0801698
CaelaMc1 <caela.mccartney@snhu.edu>
2020-02-20 13:03:19 -0500

    open a filebrowser

--- GUI_Layout.py
+++ GUI_Layout.py
@@ -8,32 +8,6 @@ from kivy.graphics import Color, Ellipse, Line
 from kivy_garden.filebrowser import FileBrowser
 from kivy.uix.popup import Popup
 
-class UploadPopup(Popup):
-    def __init__(self, short_text='heading'):
-
-        browser = FileBrowser(select_string='Select', cancel_state='down')
-        browser.bind(on_success=self._fbrowser_success,
-                     on_canceled=self._fbrowser_canceled,
-                     on_submit=self._fbrowser_submit)
-
-        self.popup = Popup(
-            title=short_text,
-            content=browser, size_hint=(0.9, 0.9),
-            auto_dismiss=False
-        )
-        self.popup.open()
-
-    def _fbrowser_canceled(self, instance):
-        print('cancelled, Close self.')
-        self.popup.dismiss()
-
-    def _fbrowser_success(self, instance):
-        print(instance.selection)
-        self.popup.dismiss()
-
-    def _fbrowser_submit(self, instance):
-        print(instance.selection)
-        self.popup.open()
 class Background(Widget):
 
     def on_touch_down(self, touch):
@@ -46,13 +20,20 @@ class Background(Widget):
         if not self.collide_point(*touch.pos):
             touch.ud['line'].points += [touch.x, touch.y]
 
-
-    def fire_popup(self):
-        pops=UploadPopup()
-        pops.open()
 class Test(TabbedPanel):
     def pencil_button(self):
         Color(255, 0, 0)
+
+    def open_file_btn_pressed(self, *args):
+
+        self._fbrowser = FileBrowser(select_string='Open')
+        self._fbrowser.bind(on_success=self._file_load,
+                            on_canceled=self._cancel_popup)
+
+        self._popup = Popup(title='Open File', content=self._fbrowser,
+                            size_hint=(0.9, 0.9), auto_dismiss=False)
+
+        self._popup.open()
 
 
 class MyMain(Widget):
