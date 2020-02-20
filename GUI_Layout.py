5fa107ba0c452b0076a0307613fbb628f88f4b42
CaelaMc1 <caela.mccartney@snhu.edu>
2020-02-19 19:39:19 -0500

    open a filebrowser, hopefully

--- GUI_Layout.py
+++ GUI_Layout.py
@@ -5,8 +5,35 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse, Line
+from kivy_garden.filebrowser import FileBrowser
+from kivy.uix.popup import Popup
 
+class UploadPopup(Popup):
+    def __init__(self, short_text='heading'):
 
+        browser = FileBrowser(select_string='Select', cancel_state='down')
+        browser.bind(on_success=self._fbrowser_success,
+                     on_canceled=self._fbrowser_canceled,
+                     on_submit=self._fbrowser_submit)
+
+        self.popup = Popup(
+            title=short_text,
+            content=browser, size_hint=(0.9, 0.9),
+            auto_dismiss=False
+        )
+        self.popup.open()
+
+    def _fbrowser_canceled(self, instance):
+        print('cancelled, Close self.')
+        self.popup.dismiss()
+
+    def _fbrowser_success(self, instance):
+        print(instance.selection)
+        self.popup.dismiss()
+
+    def _fbrowser_submit(self, instance):
+        print(instance.selection)
+        self.popup.open()
 
 class Test(TabbedPanel):
     def on_touch_down(self, touch):
@@ -17,12 +44,14 @@ class Test(TabbedPanel):
     def on_touch_move(self, touch):
         touch.ud['line'].points += [touch.x, touch.y]
 
+    def fire_popup(self):
+        pops=UploadPopup()
+        pops.open()
 
 class MyWidget(BoxLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
 
 class CanvasApp(App):
 
