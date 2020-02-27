a08729b3511e55528eb81cda2c18c393f66152bf
PeterBaker0213 <peter.baker@snhu.edu>
2020-02-27 10:43:38 -0500

    Revert "save button hopefully"
    This reverts commit 3bfc472e315e95ef283bddac89bc7b3c305a299b.

--- GUI_Layout.py
+++ GUI_Layout.py
@@ -6,7 +6,6 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.widget import Widget
 from kivy.properties import ListProperty
 from kivy.graphics import Color, Ellipse, Line
-from kivy.uix.popup import Popup
 from kivy.uix.slider import Slider
 
 # Create a global variable that will hold the color of the pencil
@@ -27,6 +26,25 @@ class MyMain(Widget):
         global paint_color
         paint_color = [0, 0, 0, 1]  # Set pencil color to black
 
+        browser = FileBrowser(select_string='Select', cancel_state='down')
+        browser.bind(on_success=self._fbrowser_success,
+                     on_canceled=self._fbrowser_canceled,
+                     on_submit=self._fbrowser_submit)
+
+        self.popup = Popup(
+            
+            content=browser, size_hint=(0.9, 0.9),
+            auto_dismiss=False
+        )
+        self.popup.open()
+
+    def _fbrowser_canceled(self, instance):
+        print('cancelled, Close self.')
+        self.popup.dismiss()
+
+    def _fbrowser_success(self, instance):
+        print(instance.selection)
+        self.popup.dismiss()
     def slider(self):
         global rad
         rad = 30
@@ -62,11 +80,18 @@ class Background(Widget):
             touch.ud["line"] = Line(points=(touch.x, touch.y))
         touch.ud["line"].points += [touch.x, touch.y]
 
-    def save(self, obj):
-        self.Background().export_to_png("screenshot.png")
 
 class Test(TabbedPanel):
-    pass
+
+    def open_file_btn_pressed(self, *args):
+        self._fbrowser = FileBrowser(select_string='Open')
+        self._fbrowser.bind(on_success=self._file_load,
+                            on_canceled=self._cancel_popup)
+
+        self._popup = Popup(title='Open File', content=self._fbrowser,
+                            size_hint=(0.9, 0.9), auto_dismiss=False)
+
+        self._popup.open()
 
 
 class Test(TabbedPanel):  # Creates tab panel, all of it is done in kivy that is why we pass
