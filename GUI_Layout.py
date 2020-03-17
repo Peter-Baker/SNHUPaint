7c87db0f78254b3e250015d4f4d71f80a3c4b189
PeterBaker0213 <peter.baker@snhu.edu>
2020-03-17 11:43:04 -0400

    More Trying to Fix Things

--- GUI_Layout.py
+++ GUI_Layout.py
@@ -1,3 +1,5 @@
+from tkinter import TRUE, ARC
+
 from kivy.app import App
 
 from kivy.lang import Builder
@@ -22,8 +24,19 @@ global rad
 rad = 30
 
 
-
 class MyMain(Widget):
+    # Stores current drawing tool used
+    drawing_tool = "rectangle"
+
+    # Tracks whether left mouse is down
+    left_but = "up"
+
+    # x and y positions for drawing with pencil
+    x_pos, y_pos = None, None
+
+    # Tracks x & y when the mouse is clicked and released
+    x1_line_pt, y1_line_pt, x2_line_pt, y2_line_pt = None, None, None, None
+
     global paint_color
     paint_color = [0, 0, 0, 1]
 
@@ -46,19 +59,59 @@ class MyMain(Widget):
     def btn(self):
         show_popup()
 
-    # ---------- DRAW RECTANGLE ----------
+# ---------- CATCH MOUSE UP ----------
 
-    def rectangle_draw(self, event=None):
-        if None not in (self.x1_line_pt, self.y1_line_pt, self.x2_line_pt,                              self.y2_line_pt):
+    def left_but_down(self, event=None):
+        self.left_but = "down"
 
-            # fill : Color option names are here http://wiki.tcl.tk/37701
-            # outline : border color
-            # width : width of border in pixels
+        # Set x & y when mouse is clicked
+        self.x1_line_pt = event.x
+        self.y1_line_pt = event.y
 
-            event.widget.create_rectangle(self.x1_line_pt, self.y1_line_pt,                  self.x2_line_pt, self.y2_line_pt,
-                fill="midnight blue",
-                outline="yellow",
-                width=2)
+    # ---------- CATCH MOUSE UP ----------
+
+    def left_but_up(self, event=None):
+        self.left_but = "up"
+
+        # Reset the line
+        self.x_pos = None
+        self.y_pos = None
+
+        # Set x & y when mouse is released
+        self.x2_line_pt = event.x
+        self.y2_line_pt = event.y
+
+        # If mouse is released and line tool is selected
+        # draw the line
+        if self.drawing_tool == "line":
+            self.line_draw(event)
+        elif self.drawing_tool == "arc":
+            self.arc_draw(event)
+        elif self.drawing_tool == "oval":
+            self.oval_draw(event)
+        elif self.drawing_tool == "rectangle":
+            self.rectangle_draw(event)
+        elif self.drawing_tool == "text":
+            self.text_draw(event)
+
+    # ---------- CATCH MOUSE MOVEMENT ----------
+
+    def motion(self, event=None):
+
+        if self.drawing_tool == "pencil":
+            self.pencil_draw(event)
+
+    # ---------- DRAW PENCIL ----------
+
+    def pencil_draw(self, event=None):
+        if self.left_but == "down":
+
+            # Make sure x and y have a value
+            if self.x_pos is not None and self.y_pos is not None:
+                event.widget.create_line(self.x_pos, self.y_pos,                        event.x, event.y, smooth=TRUE)
+
+            self.x_pos = event.x
+            self.y_pos = event.y
 
     # ---------- DRAW LINE ----------
 
@@ -82,6 +135,22 @@ class MyMain(Widget):
                                         outline="yellow",
                                         width=2)
 
+    # ---------- DRAW RECTANGLE ----------
+
+    def rectangle_draw(self, event=None):
+        print("Made it into function")
+        if None not in (self.x1_line_pt, self.y1_line_pt, self.x2_line_pt,                              self.y2_line_pt):
+            print("Made it into if")
+            # fill : Color option names are here http://wiki.tcl.tk/37701
+            # outline : border color
+            # width : width of border in pixels
+
+            event.widget.create_rectangle(self.x1_line_pt, self.y1_line_pt,                  self.x2_line_pt, self.y2_line_pt,
+                fill="midnight blue",
+                outline="yellow",
+                width=2)
+
+
 
 
 class TextInputPopup(FloatLayout):
@@ -102,22 +171,21 @@ class Background(Widget):
             if not self.collide_point(*touch.pos):  # Make sure click is on white canvas, and not TabbedPanel
                 Color(rgba=self.paint)  # Set color to paint_color value
                 global rad
-                Ellipse(pos=(touch.x, touch.y), size=(rad/2, rad/2))
+                Ellipse(pos=(touch.x, touch.y), size=(rad / 2, rad / 2))
                 touch.ud['line'] = Line(points=(touch.x, touch.y), width=15)
 
     def on_touch_move(self, touch):
         if "line" not in touch.ud:
             global rad
-            touch.ud["line"] = Line(points=(touch.x, touch.y), width=rad/2)
-        touch.ud["line"].width = rad/3.9
+            touch.ud["line"] = Line(points=(touch.x, touch.y), width=rad / 2)
+        touch.ud["line"].width = rad / 3.9
         touch.ud["line"].points += [touch.x, touch.y]  # Points are the position
-
 
 
 def show_popup():
     show = TextInputPopup()
 
-    popupWindow = (Popup(title="Enter Text", content=show, size_hint=(None, None), size=(400,200)))
+    popupWindow = (Popup(title="Enter Text", content=show, size_hint=(None, None), size=(400, 200)))
 
     popupWindow.open()
 
